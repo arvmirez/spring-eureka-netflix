70a5d4584bfd3d29d2948123fd7cad32f52a738b
arvmirez <aramirez@pagantis.com>
2020-02-06 15:30:13 +0100

    added spring initializr project

---
+++ src/main/java/com/arvmirez/springeurekanetflix/SpringEurekaNetflixApplication.java
@@ -0,0 +1,13 @@
+package com.arvmirez.springeurekanetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaNetflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arvmirez/springeurekanetflix/SpringEurekaNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvmirez.springeurekanetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
